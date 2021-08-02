54361cb1d6774b10d7c01936cf99e5fc5c109473
shenqianjin <qianjinshen@microsoft.com>
2021-08-02 13:37:54 +0800

    update.

--- boot-demo/demo-boot-01/src/main/java/com/example/demo/DemoApplication.java
+++ boot-demo/demo-boot-01/src/main/java/com/example/demo/DemoApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		if (System.currentTimeMillis() % 2 >= 0) {
+			throw new RuntimeException("test boot error in spring boot application.");
+		}
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
--- boot-demo/demo-boot-01/src/main/java/com/example/demo/controller/DemoController.java
+++ boot-demo/demo-boot-01/src/main/java/com/example/demo/controller/DemoController.java
@@ -1,29 +1,14 @@
 package com.example.demo.controller;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
 
-    @Value("${qianjinEnv1:}")
-    private String qianjinEnv1;
-    @Value("${qianjinEnv2:}")
-    private String qianjinEnv2;
-
     @RequestMapping({"/", "/index"})
     public String myIndex() {
-        /*System.out.println("qianjin1" + " ---> " + System.getProperty("qianjin1"));
-        System.out.println("qianjin2" + " ---> " + System.getProperty("qianjin2"));*/
-        System.out.println("========================");
-        System.out.println("qianjinEnv1" + " ---> " + qianjinEnv1);
-        System.out.println("qianjinEnv2" + " ---> " + qianjinEnv2);
-
-        /*System.out.println("qianjinEnv1 by System.getProperty" + " ---> " + System.getProperty("qianjinEnv1"));*/
-        System.out.println("envFromExtension2" + " ---> " + System.getenv("envFromExtension2"));
-        System.out.println("qianjinEnv1 by System.getEnv" + " ---> " + System.getenv("qianjinEnv1"));
-        return "Hello Spring Boot. ---- NEW ---one deploy 02 - slot";
+        return "Hello Spring Boot.";
     }
 
 }
